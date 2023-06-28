c168f2b5bd2e9b010757d3f30a85bbb5b0383109
Tarcio Teles <tarcioapps7@gmail.com>
2023-06-27 21:57:02 -0300

    add

--- 01_Kanban/main.js
+++ 01_Kanban/main.js
@@ -1,55 +1,74 @@
-const tasks = document.querySelectorAll('.task li');
+const tasks = document.querySelectorAll(".tasks li");
 let draggedTask = null;
 
-for(let i =0; i < tasks.clientHeight; i ++) {
-    const task = tasks[i];
+for (let i = 0; i < tasks.length; i++) {
+  const task = tasks[i];
 
-    task.addEventListener('dragstart', function (event) {
-        draggedTask =task;
-        event.dataTransfer.effectAllowed = 'move';
-        event.dataTransfer.setData('text/html', task.innerHTML);
-        task.classList.add('dragging');
+  task.addEventListener("dragstart", function (event) {
+    draggedTask = task;
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/html", task.innerHTML);
+    task.classList.add("dragging");
+  });
+
+  task.addEventListener("dragend", function () {
+    draggedTask.classList.remove("dragging");
+    draggedTask = null;
+  });
+}
+
+const columns = document.querySelectorAll(".tasks");
+
+for (let i = 0; i < columns.length; i++) {
+  const column = columns[i];
+
+  column.addEventListener("dragover", function (event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+    column.classList.add("dragover");
+  });
+
+  column.addEventListener("dragleave", function () {
+    column.classList.remove("dragover");
+  });
+
+  column.addEventListener("drop", function (event) {
+    event.preventDefault();
+    const task = document.createElement("li");
+    task.innerHTML = event.dataTransfer.getData("text/html");
+    task.setAttribute("draggable", true);
+    task.addEventListener("dragstart", function (event) {
+      draggedTask = task;
+      event.dataTransfer.effectAllowed = "move";
+      event.dataTransfer.setData("text/html", task.innerHTML);
+      task.classList.add("dragging");
     });
+    column.appendChild(task);
+    column.classList.remove("dragover");
 
-    task.addEventListener('dragend', function () {
-                draggedTask.classList.remove('dragging');
-                draggedTask = null;
-    })
+    // Remove task from previous column
+    const previousColumn = draggedTask.parentNode;
+    previousColumn.removeChild(draggedTask);
+  });
 }
 
-const coluns = document.querySelectorAll('.tasks');
+const addTaskForm = document.querySelector("#add-task-form");
+const addTaskInput = addTaskForm.querySelector("input");
 
-for (let i = 0; i < colum.length; i++) {
-    const colum = coluns[i];
-
-    colum.addEventListener('dragover', function (event) {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'move';
-        colum.classList.add('dragover');
-    })
-
-    colum.addEventListener(dragleave, function (event) {
-        event.preventDefault();
-
-        const task =document.createElement('li');
-
-        task.innerHTML = event.dataTransfer.getData('text/html');
-        task.setAttribute('draggable', true);
-        task.addEventListener('dragstart', function (event) {
-            draggedTask = task;
-            event.dataTransfer.effectAllowed = 'move';
-            event.dataTransfer.setData('text/html', task.innerHTML);
-            task.classList.add('dragging');
-        })
-
-
-
-        colum.appendChild(task);
-        colum.classList.remove('dragover');
-
-        const previousColum = draggedTask.parentNode;
-        previousColum.removeChild(draggedTask);
-        
-    })
-
-}
+addTaskForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const newTaskText = addTaskInput.value.trim();
+  if (newTaskText !== "") {
+    const newTask = document.createElement("li");
+    newTask.innerHTML = newTaskText;
+    newTask.setAttribute("draggable", true);
+    newTask.addEventListener("dragstart", function (event) {
+      draggedTask = newTask;
+      event.dataTransfer.effectAllowed = "move";
+      event.dataTransfer.setData("text/html", newTask.innerHTML);
+      newTask.classList.add("dragging");
+    });
+    document.querySelector("#todo").appendChild(newTask);
+    addTaskInput.value = "";
+  }
+});
